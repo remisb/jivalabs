95f37b6e39d18ff7f059623906120f2ef9d5f2d1
Amungas <>
2019-05-24 10:40:14 +0300

    updated public web pages (docs dir)

---
+++ docs/js/chunk-2d0b2cf8.54c60696.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0b2cf8"],{2664:function(n,e,t){"use strict";t.r(e);var u=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("h1",[n._v("This is About us page")])},s=[],c={},l=c,a=t("2877"),i=Object(a["a"])(l,u,s,!1,null,null,null);e["default"]=i.exports}}]);
+//# sourceMappingURL=chunk-2d0b2cf8.54c60696.js.map
--- docs/readme.md
+++
@@ -1,1 +0,0 @@
-# Jiva Labs - Web Site
